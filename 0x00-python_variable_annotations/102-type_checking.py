146cbf72706d8ecf2554927e6d70593671c8ec72
Mutai05 <kelvok2014@gmail.com>
2024-06-12 09:39:41 +0000

    Task 12 - Use mypy to validate the following piece of code and apply any necessary changes.

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module for zooming an array by a given factor.
+"""
+
+from typing import List, Tuple
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """
+    Zoom in on the elements of the tuple by the given factor.
+
+    Args:
+        lst (Tuple[int, ...]): The tuple to be zoomed.
+        factor (int, optional): The zoom factor. Defaults to 2.
+
+    Returns:
+        List[int]: A list with elements repeated by the zoom factor.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[int, ...] = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
